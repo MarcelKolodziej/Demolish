a866750e46e84e2a6dd66634843996ad1b891643
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-10-03 22:48:04 +0200

    Add Lose Condition, with scene Loader.

---
+++ Demolish/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Invoke("LoadFirstlevel", 1.0f);
+    }
+    public void LoadFirstlevel()
+    {
+      SceneManager.LoadScene("First Level");
+    }
+
+}
+
