163f62e04e04c820ac245c76b2b4e46aad00e934
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-10-30 19:30:57 +0100

    Add second level

--- Demolish/Assets/Scripts/LoseCollider.cs
+++ Demolish/Assets/Scripts/LoseCollider.cs
@@ -9,10 +9,11 @@ public class LoseCollider : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Invoke("LoadFirstlevel", 1.0f);
+        Debug.Log(collision.gameObject.name);
     }
     public void LoadFirstlevel()
     {
-      SceneManager.LoadScene("First Level");
+      SceneManager.LoadScene("Game Over");
     }
 
 }
